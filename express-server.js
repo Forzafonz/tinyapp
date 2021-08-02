b41fc175157a33bd9843cf63e898d0a08d0ce72e
Anton <golubin1990@gmail.com>
2021-08-02 18:13:25 -0400

    added a new route and a link to a template

--- express-server.js
+++ express-server.js
@@ -18,14 +18,19 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render('urls_indexe.ejs', templateVars);
+  res.render('urls_index.ejs', templateVars);
 });
-
-
 
 app.get("/hello", (req, res) => {
   const test1 = {fruits: ['peach', 'pear', 'apple']};
   res.render('about', test1);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+
+  const templateVar = { 'shortURL': req.params.shortURL, 'longURL': urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVar);
+
 });
 
 app.listen(PORT, () => {
