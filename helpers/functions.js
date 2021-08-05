0a149a04d68613005545770bf5e6ebbaf1d6d454
Anton <golubin1990@gmail.com>
2021-08-05 19:28:08 -0400

    Fixing some linting issues

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,5 +1,4 @@
 const {letters, numbers} = require('../helpers/constants');
-const { urlDatabase } = require('./data');
 
 //Function to generate random strings which will be used as an shortURL
 const generateRandomString = function() {
@@ -16,7 +15,7 @@ const generateRandomString = function() {
 //Function which is used to add/update longUrl in the database based on shortUrl
 const addToDatabase = function(urlDatabase, shortUrl, longUrl, id) {
   let standUrl = getLongUrl(longUrl);
-  urlDatabase[shortUrl] = {longURL:standUrl, userID: id} ;
+  urlDatabase[shortUrl] = {longURL:standUrl, userID: id};
 
 };
 
@@ -41,7 +40,7 @@ const removeFromDatabase = function(urlDatabase, shortURL) {
 };
 
 // Function to generate userID:
-const getUniqID = function (data){
+const getUniqID = function(data) {
 
   let uniqueKey = [];
   let symbols = letters + numbers;
@@ -49,28 +48,28 @@ const getUniqID = function (data){
   uniqueKey = uniqueKey.map(element => {
     return symbols[element];
   });
-  if (data[uniqueKey.join('')] === undefined){
+  if (data[uniqueKey.join('')] === undefined) {
 
     return uniqueKey.join('');
     
   } else {
     return getUniqID(data);
-  } 
+  }
 
-}
+};
 
 //Function to add user to "database":
-const addUser = function ({data, id, email, password}) {
+const addUser = function({data, id, email, password}) {
   if (data[id] === undefined) {
     data[id] = {id, email, password};
   } else {
     return null;
-  };
+  }
 };
 
 //Function to get user id by e-mail:
 
-const getUserID = function ({data, email}) {
+const getUserID = function({data, email}) {
 
   for (let key in data) {
     if (data[key]['email'] === email) {
@@ -78,51 +77,55 @@ const getUserID = function ({data, email}) {
     }
   }
   return null;
-}
+};
 
 // Function to check and extract ID from cookies:
 
-const extractID = function (cookies) {
-  const result = cookies ? cookies : {}
-  if (cookies['userid'] !== undefined) {
-    return cookies['userid'];
+const extractID = function(cookies) {
+ 
+  const result = cookies ? cookies : {};
+  if (result['userid'] !== undefined) {
+    return result['userid'];
   }
   return null;
-}
+};
 
 // Function to check if user already exists in "users" object (for registration):
 
-const userExists = function ({email, users}) {
+const userExists = function({email, users}) {
   for (const key in users) {
-    if (users[key]['email'] === email){
+    if (users[key]['email'] === email) {
       return true;
     }
   }
   return false;
-}
+};
 
 // Function to check if a longURL includes http part in it. If not - add it.
 
-const getLongUrl = function (url) {
+const getLongUrl = function(url) {
   return url.substring(0,4) === 'http' ? url : 'https://' + url;
-}
+};
 
 // Function which returns URLS where the userID is equal to the id of the currently logged-in user:
 
-const urlsForUser = function ({data, userID}) {
+const urlsForUser = function({data, userID}) {
   let urlsForUser = {};
   let showKeys = Object.keys(data)
-                       .filter(key => {
-                         if (data[key]['userID'] === userID) {return true} })               
-        if (showKeys.length > 0) {
-        showKeys.forEach(key => {
-          urlsForUser[key] = data[key];
-        })
-      } else {
-        return null;
+    .filter(key => {
+      if (data[key]['userID'] === userID) {
+        return true;
       }
+    });
+  if (showKeys.length > 0) {
+    showKeys.forEach(key => {
+      urlsForUser[key] = data[key];
+    });
+  } else {
+    return null;
+  }
 
   return urlsForUser;
-}
+};
 
-module.exports = {generateRandomString, addToDatabase, getFromDatabase, removeFromDatabase, getUniqID, addUser, getUserID, extractID, userExists, urlsForUser}
+module.exports = {generateRandomString, addToDatabase, getFromDatabase, removeFromDatabase, getUniqID, addUser, getUserID, extractID, userExists, urlsForUser};
