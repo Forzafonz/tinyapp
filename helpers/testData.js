6d3ab150b704c8b983c871aaec3f4d945d3445ac
Anton <golubin1990@gmail.com>
2021-08-05 21:05:54 -0400

    Added a testData file to be used by Mocha. Just to keep actual and test data separate

---
+++ helpers/testData.js
@@ -0,0 +1,30 @@
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const urlDatabase = {
+
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "user2RandomID"
+  }, 
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID"
+  },
+  "sgq3y6": {
+    longURL: "https://support.mozilla.org/en-US/products/firefox",
+    userID: "userRandomID"
+  }
+}
+
+module.exports = {users, urlDatabase}
